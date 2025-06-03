ec4f4fd380d97db9436280ceb7854bba6dc7639f
Leo-Gad <hakizimanaleogad@gmail.com>
2025-06-03 17:14:45 +0200

    Iot_System.ino
    tiny chip big project

--- iot_System.ino
+++ iot_System.ino
@@ -1,104 +1,104 @@
-/*
-Author : "leoGad"
-project-Name: "Iot System Firmware"
-Date: 30/05/2025
-Email-Address:hakizimanaleogad@gmail.com
-https://github.com/leo-gad123/Iot_System_Firmware  
-*/
-#include<DHT.h>
-#include<LiquidCrystal_I2C.h>
-#include<FirebaseESP8266.h>
-#include<DIYables_IRcontroller.h>
-#include<ESP8266WiFi.h>
-#define url "leo.firebase"
-#define TOKEN "" 
-LiquidCrystal_I2C lcd(0x27,16,2);
-DHT dht(11,11);
-String wifi ="EdNet";
-String pass ="Hauwei@123";
+#include <DHT.h>
+#include <LiquidCrystal_I2C.h>
+#include <FirebaseESP8266.h>
+#include <DIYables_IRcontroller.h>
+#include <ESP8266WiFi.h>
+
+#define url "https://dht11-9aca0-default-rtdb.firebaseio.com"
+#define TOKEN "I5fAerwYcyQrGqZ5x4JzA2v8QPQ0f2lQwXYBbJl2"
+#define DHTPIN 11
+#define DHTTYPE DHT11
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+DHT dht(DHTPIN, DHTTYPE);
+DIYables_IRcontroller_17 ir(12, 200);
+
+const char* ssid = "EdNet";
+const char* password = "Hauwei@123";
+
 FirebaseAuth auth;
 FirebaseData data;
 FirebaseConfig config;
-int led =13;
-DIYables_IRcontroller_17 ir(12,200);
-void setup(){
- 
+
+int led = 13;
+
+void setup() {
   Serial.begin(115200);
-  WiFi.begin(wifi,pass);
-  while(WiFi.status() != WL_CONNECTED){
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
     delay(1000);
   }
-  Serial.print("Wifi connected");
- 
-  config.host=url;
-  config.signer.tokens.legacy_token=TOKEN;
+  Serial.println("WiFi connected");
+
+  config.database_url = url;
+  config.signer.tokens.legacy_token = TOKEN;
   Firebase.begin(&config, &auth);
- 
+
   ir.begin();
-  pinMode(led,OUTPUT);
+  pinMode(led, OUTPUT);
   dht.begin();
   lcd.init();
-  lcd.print("ꄣSmart Home Automationꄣ");
+  lcd.backlight();
+  lcd.print("ꄣSmart Home");
+  lcd.setCursor(0, 1);
+  lcd.print(" Automation ꄣ");
   delay(3000);
   lcd.clear();
 }
-void loop(){
-   Key17 key=ir.getKey();
+
+void loop() {
+  Key17 key = ir.getKey();
+  remmote(key);
   interNet();
-
-displayOff();
-}
-void remmote(Key17 key){
-  if(key!=Key17::NONE){
-  switch(key){
-     case Key17::KEY_OK:
-     bool value = (key==Key17::KEY_OK)?1:0;
-     digitalWrite(led,value);
-     break;
-  }
- }
-}
-void displayOff(){
- lcd.print("ꄣTemp:");
- lcd.print(dht.readTemperature());
- lcd.print("c");
- lcd.print("ꄣHum:");
- lcd.print(dht.readHumidity());
- lcd.print("%");
- delay(1000);
- lcd.clear();
+  displayOff();
 }
 
-void interNet(){
-   if(WiFi.status() == WL_CONNECTED){
-if(Firebase.getString(data,"/status")){
-  String value=data.stringData();
-  if(value=="true"){
-  digitalWrite(led,1);
+void remmote(Key17 key) {
+  if (key != Key17::NONE) {
+    switch (key) {
+      case Key17::KEY_OK:
+        digitalWrite(led, HIGH);
+        break;
+      default:
+        break;
+    }
   }
-  else{
-    digitalWrite(led,0);
+}
+
+void displayOff() {
+  float temp = dht.readTemperature();
+  float hum = dht.readHumidity();
+
+  lcd.setCursor(0, 0);
+  lcd.print("Temp: ");
+  lcd.print(temp);
+  lcd.print("C");
+
+  lcd.setCursor(0, 1);
+  lcd.print("Hum: ");
+  lcd.print(hum);
+  lcd.print("%");
+
+  delay(2000);
+  lcd.clear();
+}
+
+void interNet() {
+  if (WiFi.status() == WL_CONNECTED) {
+    if (Firebase.getString(data, "/status")) {
+      String value = data.stringData();
+      digitalWrite(led, value == "true" ? HIGH : LOW);
+    }
+
+    float temp = dht.readTemperature();
+    float hum = dht.readHumidity();
+
+    Firebase.setFloat(data, "/Temperature", temp);
+    Firebase.setFloat(data, "/Humidity", hum);
+  } else {
+    Serial.println("Network disconnected");
   }
+
+  delay(1000);
 }
-else{
-  Serial.print("data not recieved ???????");
-}
-if(Firebase.setFloat(data,"/Temperature: ",dht.readTemperature())){
-  Serial.print("data sent successfully");
-}
-else{
-  Serial.print("Error??????");
-}
-if(Firebase.setFloat(data,"/Humidity: ",dht.readHumidity())){
-  Serial.print("data sent successfully");
-}
-else{
-  Serial.print("Error 🼴🼴 🼴🼴🼴🼴 🼴🼴🼴 🼴🼴🼴");
-}
- }else
-    Serial.print("nework failed");
-    delay(1000);
-}
-// Thank you for using this firmware
-//most requirement of loRa is Wifi
